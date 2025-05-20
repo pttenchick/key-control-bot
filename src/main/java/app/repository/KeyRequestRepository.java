78e2e2b1481e50451ba29367cf8221191a638e64
Pttenchick <tsiho03@gmail.com>
2025-05-20 19:25:23 +0300

    fix bags part 2

--- src/main/java/app/repository/KeyRequestRepository.java
+++ src/main/java/app/repository/KeyRequestRepository.java
@@ -4,6 +4,4 @@ import app.model.KeyRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeyRequestRepository extends JpaRepository<KeyRequest, String> {
-    KeyRequest findByKey_Id(Long keyId);
-    void deleteByKey_Id(Long keyId);
 }
